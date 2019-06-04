93358140056fc08ea88ad8f44e9338b25cc93e93
stevan <sdedovic@preparedhealth.com>
2019-06-03 21:11:26 -0500

    fix - https://github.com/zeit/now-builders/blob/master/errors/now-next-no-serverless-pages-built.md

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  webpack: config => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty'
-    }
+    target: 'serverless'
 
-    return config
-  }
+    webpack: config => {
+        // Fixes npm packages that depend on `fs` module
+        config.node = {
+            fs: 'empty'
+        }
+        return config
+    }
 }
